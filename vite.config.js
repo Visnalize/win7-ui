34b5708a324904199aeec198ef0ced839e8aaa34
Khang <khang.nguyenduyvn@gmail.com>
2022-10-28 12:05:04 +0700

    chore: config build

--- vite.config.js
+++ vite.config.js
@@ -5,17 +5,44 @@ import legacy from "@vitejs/plugin-legacy";
 import vue2 from "@vitejs/plugin-vue2";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    vue2(),
-    legacy({
-      targets: ["ie >= 11"],
-      additionalLegacyPolyfills: ["regenerator-runtime/runtime"],
-    }),
-  ],
-  resolve: {
-    alias: {
-      "@": fileURLToPath(new URL("./src", import.meta.url)),
+export default defineConfig(({ mode }) => {
+  let options = {};
+  let plugins = [];
+
+  if (mode === "lib") {
+    options = {
+      build: {
+        lib: {
+          entry: "./src/index.js",
+          name: "WinUI",
+          fileName: "index",
+        },
+        rollupOptions: {
+          external: ["vue"],
+          output: {
+            dir: "lib",
+          },
+        },
+      },
+    };
+  }
+
+  if (mode === "docs") {
+    plugins = [
+      legacy({
+        targets: ["ie >= 11"],
+        additionalLegacyPolyfills: ["regenerator-runtime/runtime"],
+      }),
+    ];
+  }
+
+  return {
+    ...options,
+    plugins: [vue2(), ...plugins],
+    resolve: {
+      alias: {
+        "@": fileURLToPath(new URL("./src", import.meta.url)),
+      },
     },
-  },
+  };
 });
